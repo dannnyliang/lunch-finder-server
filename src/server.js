c9198e46fd97de06ede025a2621f1228e06c59f5
DannyLiang <youchenliang@gmail.com>
2020-03-06 00:46:29 +0800

    feat: connect server to mongodb and insert db to apollo context

--- src/server.js
+++ src/server.js
@@ -3,33 +3,66 @@ import "dotenv/config";
 import { ApolloServer } from "apollo-server-express";
 import cors from "cors";
 import express from "express";
+import { MongoClient } from "mongodb";
 
 import { resolvers, typeDefs } from "./graphql/index";
 
-const PORT = process.env.PORT;
+const {
+  PORT,
+  PROTOCAL,
+  HOST,
+  DB_NAME,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_CLUSTERNAME
+} = process.env;
 
-const app = express();
+const connect = async () => {
+  const uri = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTERNAME}-5csrd.gcp.mongodb.net/test?retryWrites=true&w=majority`;
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true
-  })
-);
+  const db = client.db(DB_NAME);
+  console.log(`🗄 Connected to DB: ${DB_NAME}`);
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  playground: true,
-  introspection: true
-});
+  return db;
+};
 
-server.applyMiddleware({
-  app,
-  cors: false,
-  path: "/"
-});
+const runApp = db => {
+  const app = express();
 
-app.listen({ port: PORT || 4000 }, () => {
-  console.log(`🚀 Server ready on port: ${PORT}`);
-});
+  app.use(
+    cors({
+      origin: true,
+      credentials: true
+    })
+  );
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: {
+      db
+    },
+    playground: true,
+    introspection: true
+  });
+
+  server.applyMiddleware({
+    app,
+    cors: false,
+    path: "/"
+  });
+
+  return app;
+};
+
+const startServer = async () => {
+  const db = await connect();
+  const app = runApp(db);
+
+  app.listen({ port: PORT || 4000 }, () => {
+    console.log(`🚀 Apollo Server Ready on ${PROTOCAL}://${HOST}:${PORT}`);
+  });
+};
+
+startServer();
