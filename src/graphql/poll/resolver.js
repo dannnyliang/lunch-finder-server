7bc67e3e76e935e63064db87fc61129df5126fda
DannyLiang <youchenliang@gmail.com>
2020-03-12 00:07:23 +0800

    feat(poll collection): complete `giveOpinion`, `directDecide`, `abandonPoll` mutate resolver
    other changes:
    - update `abandonPoll` return type to Poll
    - add `opinions` field subResolver

--- src/graphql/poll/resolver.js
+++ src/graphql/poll/resolver.js
@@ -1,6 +1,6 @@
 import { ApolloError } from "apollo-server";
 import { ObjectId } from "mongodb";
-import { has, map, mergeDeepRight } from "ramda";
+import { has, includes, isNil, map, mergeDeepRight, reduce } from "ramda";
 
 import { insertIdField, removeObjectIdField } from "../utils";
 
@@ -67,11 +67,39 @@ const resolvers = {
         throw new ApolloError(error.message);
       }
     },
+    opinions: async (parent, variable, { db }) => {
+      try {
+        const { opinions } = parent;
+
+        const getUser = memberId =>
+          db.collection("users").findOne({ _id: ObjectId(memberId) });
+
+        const getRestaurants = restaurantIdList =>
+          db
+            .collection("restaurants")
+            .find({ _id: { $in: map(ObjectId, restaurantIdList) } })
+            .toArray();
+
+        const promiseList = map(
+          async opinion => ({
+            member: insertIdField(await getUser(opinion.member)),
+            opinion: map(insertIdField, await getRestaurants(opinion.opinion))
+          }),
+          opinions
+        );
+
+        const result = await Promise.all(promiseList);
+
+        return result;
+      } catch (error) {
+        throw new ApolloError(error.message);
+      }
+    },
     decision: async (parent, variable, { db }) => {
       try {
         const decision = await db
           .collection("restaurants")
-          .findOne({ _id: parent.decision });
+          .findOne({ _id: ObjectId(parent.decision) });
 
         return decision ? insertIdField(decision) : null;
       } catch (error) {
@@ -134,11 +162,10 @@ const resolvers = {
 
         const originalDocument = await db
           .collection("polls")
-          .find({ _id: ObjectId(id) })
-          .toArray();
+          .findOne({ _id: ObjectId(id) });
 
         const update = mergeDeepRight(
-          removeObjectIdField(originalDocument[0]),
+          removeObjectIdField(originalDocument),
           payload
         );
 
@@ -162,6 +189,101 @@ const resolvers = {
         await db.collection("polls").findOneAndDelete({ _id: ObjectId(id) });
 
         return "Remove Completed!";
+      } catch (error) {
+        throw new ApolloError(error.message);
+      }
+    },
+    giveOpinion: async (parent, variables, { db }) => {
+      try {
+        const { id, payload } = variables;
+
+        const originalDocument = await db
+          .collection("polls")
+          .findOne({ _id: ObjectId(id) });
+
+        const { opinions } = originalDocument;
+        const newOpinoins = isNil(opinions)
+          ? [payload]
+          : reduce(
+              (acc, elem) =>
+                elem.member === payload.member
+                  ? payload.opinions
+                  : elem.opinions,
+              []
+            )(opinions);
+
+        const newDocument = await db.collection("polls").findOneAndUpdate(
+          { _id: ObjectId(id) },
+          {
+            $set: {
+              ...originalDocument,
+              opinions: newOpinoins
+            }
+          },
+          { returnOriginal: false }
+        );
+
+        return insertIdField(newDocument.value);
+      } catch (error) {
+        throw new ApolloError(error.message);
+      }
+    },
+    directDecide: async (parent, variables, { db }) => {
+      try {
+        const { id, decision } = variables;
+
+        const originalDocument = await db
+          .collection("polls")
+          .findOne({ _id: ObjectId(id) });
+
+        if (originalDocument.status !== POLL_STATUS.POLLING) {
+          throw new Error("此投票非進行中，無法完成");
+        }
+        if (!includes(decision, originalDocument.options)) {
+          throw new Error("選擇的結果不在選項中，請重新選擇");
+        }
+
+        const newDocument = await db.collection("polls").findOneAndUpdate(
+          { _id: ObjectId(id) },
+          {
+            $set: {
+              ...originalDocument,
+              status: POLL_STATUS.COMPLETED,
+              decision
+            }
+          },
+          { returnOriginal: false }
+        );
+
+        return insertIdField(newDocument.value);
+      } catch (error) {
+        throw new ApolloError(error.message);
+      }
+    },
+    abandonPoll: async (parent, variables, { db }) => {
+      try {
+        const { id } = variables;
+
+        const originalDocument = await db
+          .collection("polls")
+          .findOne({ _id: ObjectId(id) });
+
+        if (originalDocument.status !== POLL_STATUS.POLLING) {
+          throw new Error("此投票非進行中，無法廢棄");
+        }
+
+        const newDocument = await db.collection("polls").findOneAndUpdate(
+          { _id: ObjectId(id) },
+          {
+            $set: {
+              ...originalDocument,
+              status: POLL_STATUS.ABANDONED
+            }
+          },
+          { returnOriginal: false }
+        );
+
+        return insertIdField(newDocument.value);
       } catch (error) {
         throw new ApolloError(error.message);
       }
